3a1a212d5d56727443cff5829231af7772a36665
massallys <massallys@hotmail.com>
2021-06-21 09:43:39 -0300

    excercise finished

--- Pyhton Curso/pythonExercicios/ex028.py
+++ Pyhton Curso/pythonExercicios/ex028.py
@@ -1,2 +1,24 @@
 #jogo de advinhação
 
+import random
+import colorama
+
+from colorama import Fore
+from time import sleep
+
+colorama.init()
+
+print(Fore.YELLOW + '-=-' * 20)
+print(Fore.BLUE + 'Vou pensar em um número entre 0 e 5. Tente adivinhar...')
+print(Fore.YELLOW + '-=-' * 20)
+
+num = int(input(Fore.RESET + 'Em que número eu pensei? '))
+print('PRONCESSANDO...')
+sleep(3)
+
+Numrand = random.randrange(0, 5)
+
+if num == Numrand:
+    print('PARABÉNS! Você conseguiu me vencer!')
+else:
+    print('GANHEI! Eu pensei no número {} e não no {}!'.format(Numrand, num))
