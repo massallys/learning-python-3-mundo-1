f3512e2faa7936a94f7215e96ead78dfbb324705
massallys <massallys@hotmail.com>
2021-06-22 00:50:43 -0300

    new exercise finished

---
+++ Pyhton Curso/pythonExercicios/ex030.py
@@ -0,0 +1,11 @@
+import colorama
+from colorama import Fore
+
+num = int(input(Fore.LIGHTRED_EX + 'Me diga um número qualquer: '))
+
+resultado = num % 2
+
+if resultado == 0:
+    print(Fore.BLUE + 'O número {} é PAR'.format(num))
+else:
+    print(Fore.BLUE + 'O número {} é ÍMPAR'.format(num))
