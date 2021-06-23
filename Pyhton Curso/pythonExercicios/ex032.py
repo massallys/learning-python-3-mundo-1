a7fdff2e9aec45155bc1ec8384684a93350eb61d
massallys <massallys@hotmail.com>
2021-06-22 23:27:21 -0300

    exercise finished it

--- Pyhton Curso/pythonExercicios/ex032.py
+++ Pyhton Curso/pythonExercicios/ex032.py
@@ -1,1 +1,12 @@
 #mostre se o ano digitado é bissexto
+
+from datetime import date
+
+ano = int(input('Que ano quer analisar? Coloque 0 para analsar o ano atual: '))
+
+if ano == 0:
+    ano = date.today().year
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print('O ano {} é BISSEXTO'.format(ano))
+else:
+    print('O ano {} NÃO É BISSEXTO!'.format(ano))
