8f718cfbd00f3c5de49902b4e3884d90d3d5017e
massallys <massallys@hotmail.com>
2021-06-23 00:01:27 -0300

    finished exercise

--- Pyhton Curso/pythonExercicios/ex033.py
+++ Pyhton Curso/pythonExercicios/ex033.py
@@ -1,1 +1,28 @@
 #mostrar quais numeros adiconados é maior ou menor
+
+num1 = int(input('Primeiro valor: '))
+num2 = int(input('Segundo valor: '))
+num3 = int(input('Terceiro valor: '))
+
+#verificar menor
+
+menor = num1
+
+if num2 < num1 and num2 < num3:
+    menor = num2
+
+if num3 < num1 and num3 < num2:
+    menor = num3
+
+#verifica maior
+
+maior = num1
+
+if num2 > num1 and num2 > num3:
+    maior = num2
+
+if num3 > num1 and num3 > num2:
+    maior = num3
+
+print('O menor valor digitado foi {}'.format(menor))
+print('O maior valor digitado foi {}'.format(maior))
