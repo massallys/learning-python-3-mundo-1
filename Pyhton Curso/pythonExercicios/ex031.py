8369ec6dc951e68f79a79fb0e1bc0736ffa65e91
massallys <massallys@hotmail.com>
2021-06-22 23:05:55 -0300

    updated it the code with few lines

--- Pyhton Curso/pythonExercicios/ex031.py
+++ Pyhton Curso/pythonExercicios/ex031.py
@@ -1,15 +1,14 @@
 #calcular a distância da viagem em km
 
 distancia = float(input('Qual é a distância da sua viagem? '))
+print('Você está prestes a começar uma viagem de {:.1f}Km.'.format(distancia))
 
 if distancia < 200:
     distanciaFinal = distancia * 0.50
 
-    print('Você está prestes a começar uma viagem de {:.1f}Km.'.format(distancia))
     print('E o preço da sua passagem será de R${:.1f}.'.format(distanciaFinal))
 else:
     distanciaFinal = distancia * 0.45
 
-    print('Você está prestes a começar uma viagem de {:.1f}Km.'.format(distancia))
     print('E o preço da sua passagem será de R${:.1f}.'.format(distanciaFinal))
 
