dd27875a22d1cf9fce1ca1587234bbb168a61b04
massallys <massallys@hotmail.com>
2021-06-23 00:26:08 -0300

    exercise finished it

--- Pyhton Curso/pythonExercicios/ex034.py
+++ Pyhton Curso/pythonExercicios/ex034.py
@@ -1,2 +1,16 @@
 #calcular o aumento de um determinado valor de salário
 
+salario = float(input('Qual é o salário do funcionário? R$'))
+
+salFinal: float = 0
+novoSal: float = 0
+
+if salario > 1250:
+    salFinal = ((salario / 100) * 10)
+    novoSal = salario + salFinal
+
+else:
+    salFinal = ((salario / 100) * 15)
+    novoSal = salario + salFinal
+
+print('Quem ganhava R${:.2f} passar a ganhar R${:.2f} agora.'.format(salario, novoSal))
